9679a7a301e7f8d3d4e5d638ee8721e4164dd2c6
José Noguera <juniornoguera1@gmail.com>
2025-07-15 09:22:52 -0600

    merge de developer a master

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ var = int(input("Inntroduce tu edad"))
 #wazaaaa
 print("hola desde mi developer")
 print("Este es un cambio más desde mi developer directo a mi rama master")
-print("Aquí arreglamos unos bugs")
+print("Aquí arreglamos unos bugs")#3
