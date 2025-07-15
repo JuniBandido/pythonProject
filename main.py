62018172e1003020acc932f8f94d25acecc5fc4d
José Noguera <juniornoguera1@gmail.com>
2025-07-15 09:20:13 -0600

    arreglo de bugs

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ print("Tercer commit")
 var = int(input("Inntroduce tu edad"))
 #wazaaaa
 print("hola desde mi developer")
+print("Este es un cambio más desde mi developer directo a mi rama master")
+print("Aquí arreglamos unos bugs")
