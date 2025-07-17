fbec7bd9a36bf0924cb2b619e5dc5c2c78cee07e
José Noguera <juniornoguera1@gmail.com>
2025-07-16 23:19:48 -0600

    Alineacion de codigo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #Aqui se hará un codigo de una calculadora en donde, para poder ser utilizada tendrá que iniciar sesion.
 #developer 2 tendrá la tarea de hacer una funcion para iniciar sesion.
 #Developer 1 tendrá la tarea de hacer una calculadora funcional.
+from fileinput import close
+
 
 def login():
     users = {123: "juan", 456: "pedro", 789: "maria"}
@@ -8,8 +10,8 @@ def login():
     if userLogin in users:
         return print(f"Bienvenid@ {users[userLogin]}")
     else:
-        return print("Incorrecto")
-      
+        return print("Incorrecto"), quit()
+
 #El developer 1 estará haciendo la funcion de la calculadora
 
 def calculator():
@@ -17,10 +19,15 @@ def calculator():
     x = int(input("Introduce el primer número\n"))
     y = int(input("Introduce el segundo número\n"))
     if op == 1:
-        print(f"El resultado es {x + y}")
+        print(f"El resultado de {x} + {y} es {x + y}")
     elif op == 2:
-            print(f"El resultado es {x - y}")
+            print(f"El resultado de {x} - {y} es {x - y}")
     elif op == 3:
-        print(f"El resultado es {x * y}")
+        print(f"El resultado de {x} * {y} es {x * y}")
     elif op == 4:
-        print(f"El resultado es {x / y}")
+        print(f"El resultado de {x} / {y} es {x / y}")
+    else:
+        print("La opcion que escogió no existe")
+
+login()
+calculator()
