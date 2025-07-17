d939e7a4c6ac8a972a484c92b209a805e012e220
José Noguera <juniornoguera1@gmail.com>
2025-07-16 22:22:57 -0600

    Reestar code

--- main.py
+++
@@ -1,9 +0,0 @@
-print("primer commit")
-print("Segundo commit")
-print("Tercer commit")
-print("Tercer commit")
-var = int(input("Inntroduce tu edad"))
-#wazaaaa
-print("hola desde mi developer")
-print("Este es un cambio más desde mi developer directo a mi rama master")
-print("Aquí arreglamos unos bugs")#3
