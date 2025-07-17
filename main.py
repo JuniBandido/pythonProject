ff18fce50cf0026983ce6a6da982bdd8be4343d2
José Noguera <juniornoguera1@gmail.com>
2025-07-16 22:47:22 -0600

    calculadora

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-print("primer commit")
-print("Segundo commit")
-print("Tercer commit")
-print("Tercer commit")
-var = int(input("Inntroduce tu edad"))
-#wazaaaa
-print("hola desde mi developer")
-print("Este es un cambio más desde mi developer directo a mi rama master")
-print("Aquí arreglamos unos bugs")
+#El developer 1 estará haciendo la funcion de la calculadora
+
+def calculator():
+    op = int(input("Qué operacion deseas hacer?\n 1. Suma\n 2. Resta\n 3. Multiplicacion\n 4. Division\n"))
+    x = int(input("Introduce el primer número\n"))
+    y = int(input("Introduce el segundo número\n"))
+    if op == 1:
+        print(f"El resultado es {x + y}")
+    elif op == 2:
+            print(f"El resultado es {x - y}")
+    elif op == 3:
+        print(f"El resultado es {x * y}")
+    elif op == 4:
+        print(f"El resultado es {x / y}")
+
+calculator()
